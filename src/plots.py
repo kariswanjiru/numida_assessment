04fedcb9d122e9fc6fb7d4af9d58772985e16097
kariswanjiru <wanjiruestar@github.com>
2024-10-09 13:55:21 +0300

    FEAT: intial plots code.

--- src/plots.py
+++ src/plots.py
@@ -1,29 +1,25 @@
 from pathlib import Path
 
-import typer
-from loguru import logger
-from tqdm import tqdm
+import click
 
-from src.config import FIGURES_DIR, PROCESSED_DATA_DIR
+from src.config import FIGURES_DIR
+from src.utils import get_logger
 
-app = typer.Typer()
+# logging.
+logger = get_logger("Make plots")
 
 
-@app.command()
+@click.command()
+@click.argument("prepocessed_path", type=click.Path(exists=True))
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
+    input_path,
     output_path: Path = FIGURES_DIR / "plot.png",
-    # -----------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Generating plot from data...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Plot generation complete.")
-    # -----------------------------------------
+    # ---- REPLACE THIS WITH YOUR OWN CODE ----
+
+    logger.info("Plot generation complete.")
 
 
 if __name__ == "__main__":
-    app()
+    main()
