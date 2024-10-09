641388c09971e5324b0437f5bf9afcc92f1e2c53
kariswanjiru <wanjiruestar@github.com>
2024-10-09 13:20:24 +0300

    FEAT: prediction and predict_probability function.

--- src/modeling/predict.py
+++ src/modeling/predict.py
@@ -1,30 +1,91 @@
+import pickle
 from pathlib import Path
 
-import typer
-from loguru import logger
-from tqdm import tqdm
+import click
+import pandas as pd
 
-from src.config import MODELS_DIR, PROCESSED_DATA_DIR
+from src.config import MODELS_DIR
+from src.utils import get_logger
 
-app = typer.Typer()
+# logging.
+logger = get_logger("Predict")
 
 
-@app.command()
+url: Path = MODELS_DIR / "rf_model.pkl"
+model = pickle.load(open(url, "rb"))
+
+
+def prediction(df):
+    pred = model.predict(df)
+    return pred[0]
+
+
+def predict_probability(df):
+    pred_proba = model.predict_proba(df)
+    return round(pred_proba[0].max() * 100, 2)
+
+
+@click.command()
+@click.argument("predictions_path", type=click.Path(exists=True))
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
-    # -----------------------------------------
+    predictions_path,
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
+    """
+    main
+
+    Predict and give prediction probability.
+
+    Parameters
+    ----------
+    predictions_path :
+        path of file with preprocessed data ready for prediction.
+
+    Returns
+    -------
+    Dataframe:
+        dataframe for a dataset that has more than one row.
+    Str:
+        text explaining results of prediction.
+
+    """
+
     logger.info("Performing inference for model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Inference complete.")
-    # -----------------------------------------
+    data = pd.read_csv(predictions_path)
+
+    if data.shape[0] == 1:
+        approval = prediction(data)
+        proba = predict_probability(data)
+
+        if approval == 0:
+            return f"The loan applied is Approved, the prediction percentage is {proba}%"
+        if approval == 2:
+            return f"The loan applied is Declined, the prediction percentage is {proba}%"
+        if approval == 3:
+            return f"The loan applied is Cancelled, the prediction percentage is {proba}%"
+        if approval == 4:
+            return f"The loan applied is Expired:, the prediction percentage is {proba}%"
+
+    else:
+        temp = data.columns
+        batch_predicitons = model.predict(temp)
+        proba_df = pd.DataFrame(
+            model.predict_proba(temp),
+            columns=[
+                "Approved",
+                "Declined",
+                "Cancelled",
+                "Expired",
+            ],
+        )
+        data["Prediction"] = batch_predicitons
+        data["Prediction"] = data["Prediction"].map(
+            {0: "Approved", 1: "Declined", 2: "Cancelled", 3: "Expired"}
+        )
+        comb_df = pd.concat([data, proba_df], axis=1)
+        return comb_df
+
+    logger.info("Inference complete.")
 
 
 if __name__ == "__main__":
-    app()
+    main()
