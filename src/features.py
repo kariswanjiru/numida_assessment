406e358864595e026b5c13dbcf3fef4ce57b343b
kariswanjiru <wanjiruestar@github.com>
2024-10-10 00:08:57 +0300

    REF: add only features selected during experimentation.

--- src/features.py
+++ src/features.py
@@ -21,6 +21,23 @@ def feature_engineering(df):
     return df
 
 
+feature_name = [
+    "approval_status" "loan_id",
+    "business_id",
+    "dismissal_description",
+    "sector",
+    "principal",
+    "total_owing_at_issue",
+    "application_number",
+    "applying_for_loan_number",
+    "loan_number",
+    "paid_late",
+    "total_recovered_on_time",
+    "total_recovered_15_dpd",
+    "cash_yield_15_dpd",
+]
+
+
 @click.command()
 @click.argument("input_path", type=click.Path(exists=True))
 @click.argument("output_path", type=click.Path())
@@ -37,17 +54,8 @@ def main(
     # Binary Values.
     data["paid_late"] = data["paid_late"].replace({False: 0, True: 1})
 
-    # Drop Columns.
-    data = data.drop(
-        columns=[
-            "credit_officer_id",
-            "acquisition_channel",
-            "employee_count",
-        ]
-    )
-
     logger.success("Features generation complete.")
-    data.to_csv(output_path, index=False)
+    data.to_csv(output_path, columns=feature_name, index=False)
 
 
 if __name__ == "__main__":
